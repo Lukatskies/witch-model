957805a4a22aac51e719ddd3427400ff9a80bc83
LUKE SHEESLEY <Luke.sheesley@uvm.edu>
2024-10-02 10:31:28 -0400

    comment

---
+++ index.js
@@ -0,0 +1,29 @@
+//witch//
+
+//Properties//
+
+//Height// - let.profile= Name:
+//Age//
+//Green Eyes//
+//Broom//
+//Hat//
+
+//Events//
+
+//Greeting//
+//Cast Spell//
+//Broom Fly//
+
+
+//Method//
+castShrinkingSpell (){
+
+
+}
+
+text ('Hello'!){
+
+}
+
+fly() {
+}
